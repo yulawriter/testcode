80332bc5d42b2fcda1384186a69904d6f4598222
karta571110 <44608844+karta571110@users.noreply.github.com>
2019-11-07 18:39:50 +0800

    加入專案檔案。

---
+++ 20191024-1/Cale.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20191024_1
+{
+    public class Cale
+    {
+
+        public int Add(int a,int b)
+        {
+            return a + b;
+        }
+        public int Sub(int a,int b)
+        {
+            return a - b;
+        }
+        public int MuL(int a, int b)
+        {
+            return a * b;
+        }
+        public float Div(float a, float b)
+        {
+            return a / b;
+        }
+    }
+}
---
+++ 20191024-1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20191024_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cale cale = new Cale();
+            int s1 = cale.Add(3, 5);
+            int s2 = cale.Sub(7, 2);
+            int s3 = cale.MuL(9, 9);
+            float s4 = cale.Div(7, 2);
+            Console.WriteLine("3+5=" + s1);
+            Console.WriteLine("7-2=" + s2);
+            Console.WriteLine("7*2=" + s3);
+            Console.WriteLine("7/2=" + s4);
+            Console.ReadKey();
+        }
+    }
+}
